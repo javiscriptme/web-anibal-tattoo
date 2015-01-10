dbba4c5ec4353515f67aaec54330616af8eabe95
Javier Negre <javi.negre@gmail.com>
2015-01-10 18:48:07 +0100

    Clean code

--- js/main.js
+++ js/main.js
@@ -71,7 +71,7 @@ var app = {
 	},
 
 	setBlockTops: function () {
-		this.startOffset = this.windowHeight / 2;
+		/*this.startOffset = this.windowHeight / 2;
 
 		for ( var i = 0 ; i < 3 ; i++ ) {
 
@@ -79,60 +79,16 @@ var app = {
 
 			this.blocks[i].start = blockTop - this.startOffset;
 			this.blocks[i].end   = (blockTop * 1.5) - this.startOffset;
-		}
+		}*/
 	},
 
 	registerAnimatedBlocks: function () {
-		var that = this;
-
-		this.blocks = [];
-
-		for ( var i = 0 ; i < 3 ; i++ ) {
-
-			this.blocks[i] = {
-				start: null,
-				end: null,
-				lines: []
-			};
-
-			d3.selectAll('.page-block-' + i + ' .animated-line').each(function (d, j) {
-				var blockLine = d3.select(this);
-				var lineLen = blockLine.node().getTotalLength();
-
-				blockLine.attr("stroke-dasharray", lineLen + " " + lineLen)
-					.attr("stroke-dashoffset", lineLen);
-
-				that.blocks[i].lines.push( { d3el: blockLine, len: lineLen } );
-
-			});
-
-		}
-
 	},
 
 	appScroll: function () {
 		var scrollTop = $(window).scrollTop();
 
 		this.els.$header.toggleClass('shown', scrollTop > 300 );
-
-		for ( var i = 0 ; i < 3 ; i++ ) {
-			if ( scrollTop >= this.blocks[i].start && scrollTop <= (this.blocks[i].end + 200) ) {
-				var perc = (scrollTop - this.blocks[i].start) / (this.blocks[i].end - this.blocks[i].start) * 100;
-				if ( perc < 0 ) { perc = 0; } else if ( perc > 100 ) { perc = 100; }
-				this.animateBlockSvgLines(this.blocks[i].lines, perc);
-			}
-		}
-	},
-
-	animateBlockSvgLines: function (lines, perc) {
-		var numLines = lines.length;
-		for ( var i = 0 ; i < numLines ; i++ ) {
-			var len = lines[i].len;
-			lines[i].d3el.transition()
-				.duration(60)
-				.ease("linear")
-				.attr('stroke-dashoffset', len - (len * perc / 100));
-		}
 	},
 
 	unfoldGalleryGrid: function () {
